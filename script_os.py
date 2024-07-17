92f5314532f51fcbd9a628b095d4fb70e365c47a
Anastasiia Strelnik <n.strelnik@mail.ru>
2024-07-17 15:33:55 +0200

    Построение пути

---
+++ script_os.py
@@ -0,0 +1,5 @@
+import os.path
+
+TMP_PATH = os.path.join(os.getcwd(), "tmp") #путь к папке с файлами
+ZIP_PATH = os.path.join(os.getcwd(), "zipped") #путь к папке с архивом
+ZIP_RESOURCES = os.path.join(os.getcwd(), "zipped", "zip_resources.zip") #путь к архиву
